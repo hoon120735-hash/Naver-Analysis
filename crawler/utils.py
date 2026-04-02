368c7f665d04d6957a277fce714e34290bcf1526
hoon120735-hash <hoon120735@gmail.com>
2026-04-02 19:47:56 +0900

    Fix regex in clean_text and add wait_short function

--- crawler/utils.py
+++ crawler/utils.py
@@ -1,6 +1,12 @@
 import re
+import time
 
 
 def clean_text(text):
-    text = re.sub(r"[^가-힣a-zA-Z0-9\s]", "", text)
+    text = re.sub(r"[^가-힣a-zA-Z0-9\s]", " ", text)
+    text = re.sub(r"\s+", " ", text)
     return text.strip()
+
+
+def wait_short(seconds=1):
+    time.sleep(seconds)
