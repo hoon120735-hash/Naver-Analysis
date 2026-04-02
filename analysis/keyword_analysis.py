ebaf8f3a1c0772a36becd5f76cb568bb1058a548
hoon120735-hash <hoon120735@gmail.com>
2026-04-02 19:44:16 +0900

    Add keyword extraction function using Counter

--- analysis/keyword_analysis.py
+++ analysis/keyword_analysis.py
@@ -1,1 +1,17 @@
-print("keyword analysis")
+from collections import Counter
+from crawler.utils import clean_text
+
+
+def extract_keywords(reviews, top_n=10):
+    words = []
+
+    for review in reviews:
+        cleaned = clean_text(review)
+        tokens = cleaned.split()
+
+        for token in tokens:
+            if len(token) >= 2:
+                words.append(token)
+
+    counter = Counter(words)
+    return counter.most_common(top_n)
