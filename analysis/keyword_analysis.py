f688daab30c7a535002352ffa2c7379f9addefa8
hoon120735-hash <hoon120735@gmail.com>
2026-04-02 19:49:45 +0900

    Add STOPWORDS filter to keyword extraction

--- analysis/keyword_analysis.py
+++ analysis/keyword_analysis.py
@@ -2,6 +2,12 @@ from collections import Counter
 from crawler.utils import clean_text
 
 
+STOPWORDS = {
+    "정말", "진짜", "그냥", "너무", "아주", "조금", "사용", "구매",
+    "상품", "제품", "생각", "느낌", "하나", "이번", "처음", "배송"
+}
+
+
 def extract_keywords(reviews, top_n=10):
     words = []
 
@@ -10,7 +16,7 @@ def extract_keywords(reviews, top_n=10):
         tokens = cleaned.split()
 
         for token in tokens:
-            if len(token) >= 2:
+            if len(token) >= 2 and token not in STOPWORDS:
                 words.append(token)
 
     counter = Counter(words)
